cb34bf0d2207f2a633479e785a0ed8eb21a02a15
Paulo Sergio Dias <paulosvdd05@gmail.com>
2023-12-05 11:57:02 -0300

    criando funcao divisao

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,12 @@
 fun main(args: Array<String>) {
     helloWorld()
     println(sum(10,12))
+   println( divisao(10f, 3.5f))
 }
 
 fun helloWorld() = println("Hello, world!");
 
 
 fun sum (a:Int, b:Int) = (a+b)
+
+fun divisao(a:Float, b:Float) = a/b
