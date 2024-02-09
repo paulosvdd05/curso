adce070514146f9630322750acff7493052987c2
Paulo Sergio Dias <paulosvdd05@gmail.com>
2024-02-09 12:47:07 -0300

    DisplayName

--- TesteUnitario/src/test/kotlin/MainTest.kt
+++ TesteUnitario/src/test/kotlin/MainTest.kt
@@ -1,9 +1,11 @@
 import org.example.countXO
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
 class MainTest {
     @Test
+    @DisplayName("Teste metodo xxoo")
     fun testCountXO(){
         Assertions.assertEquals(countXO("xxoo"), true)
     }
