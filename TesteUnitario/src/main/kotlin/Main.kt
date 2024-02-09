349967eb7cc277cecc92534859a7473bd7195667
Paulo Sergio Dias <paulosvdd05@gmail.com>
2024-02-09 12:24:50 -0300

    Criando funcao countXO

--- TesteUnitario/src/main/kotlin/Main.kt
+++ TesteUnitario/src/main/kotlin/Main.kt
@@ -1,5 +1,25 @@
 package org.example
 
+fun countXO(str: String): Boolean{
+    val str = str.lowercase();
+    var countO = 0;
+    var countX = 0;
+
+    for (i in str.indices){
+        if(str[i] == 'x'){
+            countX ++;
+        }
+        if(str[i] == 'o'){
+            countO ++;
+        }
+    }
+
+    return countX == countO && countO != 0;
+
+
+}
+
 fun main() {
-    println("Hello World!")
+    println(countXO("xxoo"));
+    println(countXO("xxo"));
 }
