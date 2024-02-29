e85761a28f3ced7aebbfc84621c835d4c0c17e78
Paulo Sergio Dias <paulosvdd05@gmail.com>
2024-02-29 09:43:17 -0300

    Any

--- TesteUnitario/src/main/kotlin/Main.kt
+++ TesteUnitario/src/main/kotlin/Main.kt
@@ -1,19 +1,8 @@
 package org.example
 
 fun main(){
-    println(media(false,10f,8f, "", false))
+   valores("")
 }
+fun valores(value: Any){
 
-fun <T, J> media( abc: J,vararg notas: T): Float{
-    var soma = 0f;
-    for(n in notas){
-        if(n is Float){
-            soma += n
-        }
-
-        if(n is String){
-
-        }
-    }
-    return  (soma/notas.size)
 }
