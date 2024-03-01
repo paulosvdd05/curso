e0133674d663685a7f12de982918b0b9f1187c8c
Paulo Sergio Dias <paulosvdd05@gmail.com>
2024-03-01 11:22:59 -0300

    criando metodos para a classe pessoa

--- OrientacaoObj/src/main/kotlin/Main.kt
+++ OrientacaoObj/src/main/kotlin/Main.kt
@@ -1,14 +1,19 @@
-class Pessoa(
-    val anoNascimento:Int,
-    var nome: String
-)
+class Pessoa( val anoNascimento:Int, var nome: String){
+    fun dormir(){
+
+    }
+    fun acordar(){
+
+    }
+}
 
 fun main() {
     // class - comportamentos e atributos
 
     //classe (instancia) objetos
     var pessoa: Pessoa = Pessoa(2005, "Paulo")
-    pessoa.anoNascimento;
+
     pessoa.nome;
+    pessoa.dormir();
 
 }
