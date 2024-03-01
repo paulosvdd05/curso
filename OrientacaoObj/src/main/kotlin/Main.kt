d23535f5f3013acd340bd396bb90ea3c5e2918e0
Paulo Sergio Dias <paulosvdd05@gmail.com>
2024-03-01 10:50:05 -0300

    adicionando o primeiro atributo para a classe Pessoa

--- OrientacaoObj/src/main/kotlin/Main.kt
+++ OrientacaoObj/src/main/kotlin/Main.kt
@@ -1,9 +1,11 @@
-class Pessoa
+class Pessoa(
+    val anoNascimento:Int
+)
 
 fun main() {
     // class - comportamentos e atributos
 
     //classe (instancia) objetos
-    var pessoa: Pessoa = Pessoa()
+    var pessoa: Pessoa = Pessoa(2005)
 
 }
